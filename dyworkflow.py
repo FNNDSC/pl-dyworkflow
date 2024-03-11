b83fe1807b0e5548197b1a6ce13f24d1e6d12e2b
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-03-11 18:48:53 -0400

    version bump to 1.1.2

--- dyworkflow.py
+++ dyworkflow.py
@@ -43,7 +43,7 @@ timenow:Callable[[], str]       = lambda:\
 
 ld_forestResult:list            = []
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 DISPLAY_TITLE = r"""
        _           _                          _     __ _
